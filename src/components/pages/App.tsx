46dc51318b3b31ce0d07a0eb7a174abaf108c824
Andrea Kreideweiss <akreideweiss@gmail.com>
2022-09-27 21:47:07 +0200

    erste elemente

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,25 @@
 import './App.css'
 
 export const App = () => {
-  return <div>{/* TOOD: implement App */}</div>
+  return (
+    <>
+      <div>
+        <div>To Do</div>
+        <div className="Suchbox">
+          <input type="text" placeholder="Aufgabe..." />
+          <button type="button">Hinzufügen</button>
+        </div>
+        <div>
+          <div className="checkbox"></div>
+          <div>Alle Aufgaben anzeigen</div>
+        </div>
+      </div>
+      <div className="listElement">
+        <div className="checkbox"></div>
+        <div className="rating"></div>
+        <div className="task">Aufgabe</div>
+        <button></button>
+      </div>
+    </>
+  )
 }
