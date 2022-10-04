abbd3740ff46b02cbd95d2b9df6307fa74d1b951
Matthias Bilgerig <matthias.bilgerig@ost.ch>
2022-10-04 21:31:00 +0200

    nach 2ter Woche

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -3,38 +3,45 @@ import './App.css'
 export const App = () => {
   return (
   <div>
-    <div className = "pageHead">
+    <div className = "page-head">
     Todo App
     </div>
-    <div className = "pageBody">
-    <div className = "filterBar">
-        <div className = "inputLine">
-            <input className= "inputField" type="text" placeholder="Aufgabe..."></input>
-            <button className= "inputButton">Hinzufuegen</button>
+    <div className = "page-body">
+    <div className = "filter-bar">
+        <div className = "input-line">
+            <input className= "input-field" type="text" placeholder="Aufgabe..."></input>
+            <button className= "input-button">Hinzufuegen</button>
         </div>
         <div>
-          <input className="filterCheckbox" type="checkbox"></input>
+          <input type="checkbox"></input>
           Alle Anzeigen
         </div>
       </div>
-      <div className = "itemsList">
-        <div className = "singleItem">
-          <input className = "itemCheckbox" type="checkbox"></input>
-          <div className = "itemRating">🗲🗲🗲</div>
-          <div className = "itemDescription">Wohnung aufräumen</div>
-          <button className= "itemButton">Löschen</button>
+      <div className = "items-list">
+        <div className = "single-item">
+        <div></div>
+          <div>Wichtigkeit   
+          </div>
+          <div>Aufgabe   
+          </div>
         </div>
-        <div className = "singleItem">
-          <input className = "itemCheckbox" type="checkbox"></input>
-          <div className = "itemRating">🗲🗲</div>
-          <div className = "itemDescription">Lasagne kochen</div>
-          <button className= "itemButton">Löschen</button>
+        <div className = "single-item">
+          <input className = "item-checkbox" type="checkbox"></input>
+          <div className = "item-rating">🗲🗲🗲</div>
+          <div className = "item-description">Wohnung aufräumen</div>
+          <button className= "item-button">Löschen</button>
         </div>
-        <div className = "singleItem">
-          <input className = "itemCheckbox" type="checkbox"></input>
-          <div className = "itemRating">🗲</div>
-          <div className = "itemDescriptionDone">Wäsche waschen</div>
-          <button className= "itemButton">Löschen</button>
+        <div className = "single-item">
+          <input className = "item-checkbox" type="checkbox"></input>
+          <div className = "item-rating">🗲🗲🗲</div>
+          <div className = "item-description">Lasagne kochen</div>
+          <button className= "item-button">Löschen</button>
+        </div>
+        <div className = "single-item">
+          <input className = "item-checkbox" type="checkbox"></input>
+          <div className = "item-rating">🗲🗲🗲</div>
+          <div className = "item-description">Wäsche waschen</div>
+          <button className= "item-button">Löschen</button>
         </div>
       </div>
     </div>
